223bff069ecbae104f473796c54580cbe1035d41
Xinmei Xu <xu.xinmei2467@gmail.com>
2025-01-19 14:37:44 -0800

    prework completed

--- iOS101_Prework/ViewController.swift
+++ iOS101_Prework/ViewController.swift
@@ -8,7 +8,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBAction func changeBackgroundColor() {
+        view.backgroundColor = changeColor()
+    }
+    
+    func changeColor() -> UIColor{
+        
+        let red = CGFloat.random(in: 0...1)
+        let blue = CGFloat.random(in: 0...1)
+        let green = CGFloat.random(in: 0...1)
+        
+        return UIColor(red: red, green: green, blue: blue, alpha: 0.5)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
